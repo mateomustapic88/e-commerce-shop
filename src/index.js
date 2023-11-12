2c15662e12bbee7baec6c3cdf8f4a2f59514a57b
mateomustapic88 <mateo.mustapic88@gmail.com>
2023-11-12 18:59:48 +0100

    commit sidebarContext

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 // product provider
-import ProductProvider from './contexts/ProductContext';
+import ProductProvider from "./contexts/ProductContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// sidebar provider
+import SidebarProvider from "./contexts/SidebarContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
 );
