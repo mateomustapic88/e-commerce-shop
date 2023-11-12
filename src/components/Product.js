a929637ae54372caf311d68d43e5d11d30f6f0a1
mateomustapic88 <mateo.mustapic88@gmail.com>
2023-11-12 16:22:56 +0100

    commit product category, title and price

--- src/components/Product.js
+++ src/components/Product.js
@@ -37,6 +37,14 @@ const Product = ({ product }) => {
           </Link>
         </div>
       </div>
+      <div>
+        {/** category, title, price */}
+        <div className='text-sm capitalize text-gray-500 mb-1'>{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className='font-semibold mb-1'>{title}</h2>
+        </Link>
+        <div className='font-semibold'>$ {price}</div>
+      </div>
     </div>
   );
 };
