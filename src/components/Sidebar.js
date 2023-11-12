5fc442e57954f56c46855ce52cb93745b192cc18
mateomustapic88 <mateo.mustapic88@gmail.com>
2023-11-12 22:00:31 +0100

    clearCart functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,7 +14,7 @@ import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
   console.log(useContext(CartContext));
 
   return (
@@ -36,6 +36,21 @@ const Sidebar = () => {
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
+      </div>
+      <div>
+        <div className='bg-gray-200 flex w-full justify-between items-center'>
+          {/** total */}
+          <div className='uppercase font-semibold'>
+            <span className='mr-2'>Total: </span> $ 1000
+          </div>
+          {/** clear cart icon */}
+          <div
+            onClick={() => clearCart()}
+            className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl'
+          >
+            <FiTrash2 />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -42,8 +42,14 @@ const CartProvider = ({ children }) => {
     setCart(newCart);
   };
 
+  const clearCart = () => {
+    setCart([]);
+  };
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
